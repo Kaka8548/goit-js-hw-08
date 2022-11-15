efab6cbbe59e313063f4b3892007e7493d20c01c
Kaka8548 <katyakatya8548@gmail.com>
2022-11-15 21:23:25 +0400

    Завдання 1: код взято з попереднього дз, підключено бібліотеку SimpleLightBox до проєкту

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryDivEl = document.querySelector('.gallery');
+
+const addImgsToGallery = arr => {
+  const imgList = arr
+    .map(el => {
+      const imgEl = `<a class='gallery__item' href='${el.original}'><img class='gallery__image' src='${el.preview}' alt='${el.description}'></a>`;
+
+      return imgEl;
+    })
+    .join('');
+
+  galleryDivEl.insertAdjacentHTML('afterbegin', imgList);
+};
+
+addImgsToGallery(galleryItems);
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
