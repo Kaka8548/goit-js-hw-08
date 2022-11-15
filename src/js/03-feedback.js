fbc99c86cb00198ed07e11a7c956082e1f136a7a
Kaka8548 <katyakatya8548@gmail.com>
2022-11-16 00:42:52 +0400

    Завдання 3: встановлено відслідковування події вводу тексту користувачем та зберігання даних у локальному сховищі; встановлено очищення форми після події submit та видалення даних з сховища

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const userData = {};
+
+const onFormElInput = event => {
+  event.preventDefault();
+  const { target } = event;
+
+  userData[target.name] = target.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(userData));
+};
+
+const fillFormElements = () => {
+  if (localStorage.getItem('feedback-form-state') !== null) {
+    try {
+      const dataToFill = JSON.parse(
+        localStorage.getItem('feedback-form-state')
+      );
+      for (const prop in dataToFill) {
+        formEl.elements[prop].value = dataToFill[prop];
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+};
+
+fillFormElements();
+
+const onFormElSumbit = event => {
+  event.preventDefault();
+  const userEmail = formEl.elements.email.value;
+  const userMessage = formEl.elements.message.value;
+  console.log(`User email: ${userEmail}, message: ${userMessage}`);
+
+  event.target.reset();
+  localStorage.removeItem('feedback-form-state');
+};
+
+formEl.addEventListener('input', throttle(onFormElInput, 500));
+formEl.addEventListener('submit', onFormElSumbit);
