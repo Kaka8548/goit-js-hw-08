3663e3665ee83d5d87ad119ef46b2acfe7663cb2
Kaka8548 <katyakatya8548@gmail.com>
2022-11-15 22:48:01 +0400

    Завдання 2: ініціалізовано плеєр у файлі js; додано відслідковування часу на відео та додавання даних у локальне сховище; додано відновлення відео з того місця, на якому воно було зупинене, при оновленні сторінки

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+let currentTimeData = localStorage.getItem('videoplayer-current-time');
+
+try {
+  let currentTime = JSON.parse(currentTimeData);
+  player.setCurrentTime(currentTime);
+} catch (error) {
+  console.log(error.name);
+  console.log(error.message);
+}
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(data.seconds)
+    );
+  }, 1000)
+);
